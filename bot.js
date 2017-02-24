c6ed9d826a52c69145873a4f903ca46b19da3c17
Jonatha Lima <jonathalimax@gmail.com>
2017-02-24 10:33:57 -0300

    Added start command

--- bot.js
+++ bot.js
@@ -5,8 +5,18 @@ var Bot = require('node-telegram-bot-api'),
 
 console.log('bot server started...');
 
-bot.onText(/^\/say_hello (.+)$/, function (msg, match) {
-console.log('work');
+// Start
+bot.onText(/\/start (.+)/, function (msg, match) {
+
+  var welcome = 'Seja bem vindo ao Bot de marcação de consulta da OdontoSystem' +
+    + 'Para mais informações acesse: www.odontosystem.com.br';
+  bot.sendMessage(msg.chat.id, welcome).then(function () {
+    // reply sent!
+  });
+});
+
+bot.onText(/\/say_hello (.+)/, function (msg, match) {
+
   var name = match[1];
   bot.sendMessage(msg.chat.id, 'Hello ' + name + '!').then(function () {
     // reply sent!
