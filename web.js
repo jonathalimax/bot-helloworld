b6203106f55740e23784a8b7c228c5a197a15ebf
Jonatha Lima <jonathalimax@gmail.com>
2017-02-24 09:46:21 -0300

    Correction to port

--- web.js
+++ web.js
@@ -7,15 +7,9 @@ app.get('/', function(req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var PORT = process.env.PORT || 3000;
+var server = app.listen(process.env.PORT, function() {
+  var host = server.address().adress;
+  var port = server.address().port;
+});
 
-if (PORT != 3000) {
-  var server = app.listen(PORT, function() {
-    var host = server.address().adress;
-    var port = server.address().port;
-  });
-
-  console.log('Web server started at http://%s:%s', host, port);
-} else {
-  console.log('Web server started at http://%s', PORT);
-}
+console.log('Web server started at http://%s:%s', host, port);
